c4fa7e0cd8231024ba0afd3fadc061f90183be05
grock2015 <guirochaalves@yandex.com>
2016-09-29 17:25:48 -0300

    Atualizado checar_palavra.py
    Nessa atualização, removi alguns textos, caracteres e encurtei a variável total_de_letras

--- checar_palavra.py
+++ checar_palavra.py
@@ -1,28 +1,30 @@
 #!/usr/bin/env python
 
-# Exercicio que checa uma palavra, retira os dois ultimos caracteres e faz a contagem de letras restantes
+# Exercicio que checa uma palavra
+# retira os dois ultimos caracteres 
+# e retorna a mensagem definida
 
 # Funcao checa a palavra  e retorna a qtde de caracteres exceto os 2 ultimos
 def checa_palavra(palavra):
 	palavra_sem_dois_ultimos_caracteres = palavra[0:-2]
 
-  # Contar o total de letras da palavra sem os 2 ultimos caracteres
+  	# Contar a quantidade de letras e armazena o resultado
 	total_de_letras = 0
 	for i in palavra_sem_dois_ultimos_caracteres:
-		total_de_letras = total_de_letras + 1 # resumido: total_de_letras += 1
+		total_de_letras += 1
 
-	# Agora vamos checar o tamanho total das letras contadas e retorna a mensagem definida	
+	# Retorna a mensagem de acordo com o total de letras
 	if total_de_letras <= 10:
-		return "Menor que 10! Palavra: %s" % palavra_sem_dois_ultimos_caracteres
+		return "Menor que 10!"
 	elif total_de_letras <= 50:
-		return "Menor que 50! Palavra: %s" % palavra_sem_dois_ultimos_caracteres
+		return "Menor que 50!"
 	elif total_de_letras >= 100:
-		return "Maior que 100! Palavra: %s" % palavra_sem_dois_ultimos_caracteres
+		return "Maior que 100!"
 	else:
-		return "Menor que 100! Palavra: %s" % palavra_sem_dois_ultimos_caracteres
+		return "Menor que 100!"
 
-# Mensagem que obtem a palavra e joga na variavel
+# Obtem a palavra e armazena
 recebe_palavra = raw_input('Digite a palavra: ')
 
-# Imprime o retorno da funcao
+# Resultado
 print checa_palavra(recebe_palavra)
